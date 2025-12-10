e2eba1af40bed3c45f3493331dfa2257f7a247d2
Kiuliumov <79997432+Kiuliumov@users.noreply.github.com>
2025-12-10 20:42:16 +0200

    Update database.js

--- database.js
+++ database.js
@@ -1,8 +1,17 @@
-const provider = require('sqlite3');
+const { createNotesTable } = require('./database_queries')
+
+const provider = require('sqlite3').verbose();
 
 class Database {
 
     Database(){
+        const db = provider.Database('./notes.db', (err) => {
+            if (err) console.error(err.message);
+            else console.log('Connected to the notes database.');
+        });
 
+
+        db.run(createNotesTable)
     }
 }
+
